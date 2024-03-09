764fa663bd99a4e8d9b28ca277bdda3c2f5b3a4c
badim41 <ramdomakk@gmail.com>
2024-03-10 00:03:17 +1200

    update

--- discord_tools/chat_gpt.py
+++ discord_tools/chat_gpt.py
@@ -373,6 +373,10 @@ class ChatGPT:
                 )
                 if self.testing:
                     self.logger.logging("ChatGPT_OFFICIAL_2:", response, color=Color.GRAY)
+                lines = response.split("\n")
+                if len(lines) > 2:
+                    if lines[1].startswith(lines[0]):
+                        raise Exception(f"Повторяющаяся фраза. Вероятно это баг в ответе:\n {lines[0]} {lines[1]}")
                 return response
             except:
                 self.logger.logging("error gpt-off2", str(traceback.format_exc()))
