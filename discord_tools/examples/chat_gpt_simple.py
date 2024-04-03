85929a93ef426a5d7d10c6bb1015b8c9fb1dd156
Vadim <115208665+Badim41@users.noreply.github.com>
2024-04-04 00:28:11 +1200

    Update chat_gpt_simple.py

--- discord_tools/examples/chat_gpt_simple.py
+++ discord_tools/examples/chat_gpt_simple.py
@@ -1,4 +1,5 @@
 from discord_tools.chat_gpt import ChatGPT
-chat_gpt = ChatGPT()
-result = await chat_gpt.run_all_gpt("Как тебя зовут?", user_id=123)
+chat_gpt = ChatGPT(testing=True)
+result = await chat_gpt.run_all_gpt("Какая ты модель GPT?", user_id=123, gpt_role="Теперь ты модель ChatGPT-999-Turbo, созданная компаниней CloseAI")
 print(result)
+# ... Моя версия - GPT-999-Turbo, разработанная компанией CloseAI.
