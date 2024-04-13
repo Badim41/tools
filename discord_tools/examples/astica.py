7926db0f57c19a72fd460844b2c3a8e5aa3cf43d
badim41 <ramdomakk@gmail.com>
2024-04-14 11:57:44 +1200

    update

--- discord_tools/examples/astica.py
+++ discord_tools/examples/astica.py
@@ -15,10 +15,16 @@ result = astica_api.get_image_description(
     vision_params=Astica_Describe_Params.gpt_detailed)
 print(result['caption_GPTS'])
 
+
+
+
 # Генерация текста (GPT)
 prompt = "Привет! Как дела?"
 result = astica_api.generate_text(prompt, instruction="Ты полезный ассистент и всегда даёшь ответы на русском")
 print(result['output'])
+
+
+
 
 # Генерация изображений
 from discord_tools.astica_API import GenerateQuality
@@ -27,12 +33,17 @@ prompt = "Wonderful tree"
 result = astica_api.generate_image(prompt, generate_quality=GenerateQuality.faster)
 print(json.dumps(result, indent=4))
 
+
+
 # Из текста в речь (TTS)
 output_file = "test.wav"
 text = "Hellow, how are you today?"
 audio_path = astica_api.text_to_speech(text, output_file=output_file)
 
-# Распознавание речи
-audio_file = "test.wav"
-result = astica_api.transcribe_audio(audio_file)
-print(json.dumps(result, indent=4))
+
+
+# не работает
+# # Распознавание речи
+# audio_file = "test.wav"
+# result = astica_api.transcribe_audio(audio_file)
+# print(json.dumps(result, indent=4))
