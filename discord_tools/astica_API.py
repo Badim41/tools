8e1bfd0a352d7b29c9b5eb509d5ea8d04ca91e6f
badim41 <ramdomakk@gmail.com>
2024-04-14 12:07:38 +1200

    update

--- discord_tools/astica_API.py
+++ discord_tools/astica_API.py
@@ -866,16 +866,10 @@ class Astica_Free_API_key:
 
         key = "ur_api_key"
         api_key = Astica_Free_API_key.find_key(response.text, rf"{key}\s*=\s*'([^']*)'")
+
         if not api_key:
-            if error == 3:
-                with open("temp_response_key.txt", "w", encoding='utf-8') as writer:
-                    writer.write(response.text)
-                    writer.write(self.cookie)
-                    writer.write(self.accesstok)
-                raise Exception("Не удалось получить ключ")
-            time.sleep(3)
-            self.accesstok, self.ret, self.cookie = self.get_access_tokens()
-            return self.get_token(error=error + 1)
+            return self.accesstok
+
         print("refresh API key", api_key)
         return api_key
 
