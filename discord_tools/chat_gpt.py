f4666231e95d67f260448b1f3c07958d48cdab15
badim41 <ramdomakk@gmail.com>
2024-04-20 14:30:04 +1200

    update

--- discord_tools/chat_gpt.py
+++ discord_tools/chat_gpt.py
@@ -339,12 +339,8 @@ class ChatGPT:
 
         # CHAT GPT 4 (BEST OF THE BEST)
         if self.chat_gpt_4:
-            if len(prompt) > 1100:
-                prompt = prompt[:1100]
-                self.logger.logging(f"Cut prompt: ...{prompt[1050:1100]}...", color=Color.YELLOW)
-
             chat_history_temp = chat_history
-            messages = trim_history(chat_history_temp, max_length=5000)
+            messages = trim_history(chat_history_temp, max_length=15000)
             answer = await asyncio.to_thread(self.chat_gpt_4.ask_gpt, prompt, model=GPT_Models.gpt_4, attempts=3, image_path=None, chat_history=messages)
 
             if answer:
