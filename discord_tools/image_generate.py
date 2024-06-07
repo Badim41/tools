3f106e8153e9c5662be58cddd068bb92b365cd4b
Vadim <115208665+Badim41@users.noreply.github.com>
2024-06-08 08:04:16 +1200

    Update image_generate.py

--- discord_tools/image_generate.py
+++ discord_tools/image_generate.py
@@ -823,7 +823,7 @@ class ArtbreederImageGenerateAPI:
 
                 reference_images = []
                 for i, image_path in enumerate(input_image_path):
-                    reference_images.append({"data":image_to_base64(image_path, "weight":weights[i], "referenceType":"content"})
+                    reference_images.append({"data":image_to_base64(image_path), "weight":weights[i], "referenceType":"content"})
 
                 width, height = resize_image_if_small_or_big(input_image_path[0], return_pixels=True, max_megapixels=1)
                 image_path = self.artbreeder_api.merge_images(reference_images=reference_images, prompt=prompt, output_path=image_path, seed=seed,
